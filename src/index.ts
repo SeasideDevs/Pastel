729477c1cad19462e3b3d7e8d456197acf2148a1
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-08-08 20:52:46 +0000

    Finish route handler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import { readdirSync } from "fs";
 
 const server = fastify({
   ignoreTrailingSlash: true,
@@ -8,8 +9,15 @@ server.get("/", async (request, reply) => {
   return "pong\n";
 });
 
+const routes = readdirSync(__dirname + "/routes");
+routes.forEach(async (routeFileName) => {
+  const routeFile = await import(`${__dirname}/routes/${routeFileName}`);
+  const route = routeFile.route;
+  server.route(route);
+});
+
 server.listen(3000, (err, address) => {
-  if (err !== undefined) {
+  if (err) {
     console.error(err);
     process.exit(1);
   }
